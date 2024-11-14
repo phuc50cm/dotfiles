fe6d2c733fc73581017f6485702e7c5bfe793ce3
phuc50centimeters <phuc.50centimeters@gmail.com>
2024-11-14 10:41:41 +0700

    Add auto complement

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,56 +1,64 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd", "jinja_lsp", "html", "ts_ls", "pyright" }
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require('lspconfig')
-      lspconfig.lua_ls.setup {
-        settings = {
-        ['lua_ls'] = {},
-        },
-      }
-      lspconfig.clangd.setup {
-        settings = {
-        ['clangd'] = {},
-        },
-      }
-      lspconfig.jinja_lsp.setup {
-        settings = {
-        ['jinja_lsp'] = {},
-        },
-      }
-      lspconfig.html.setup {
-        settings = {
-        ['html'] = {},
-        },
-      }
-      lspconfig.ts_ls.setup {
-        settings = {
-        ['ts_ls'] = {},
-        },
-      }
-      lspconfig.pyright.setup {
-        settings = {
-        ['pyright'] = {},
-        },
-      }
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "clangd", "jinja_lsp", "html", "ts_ls", "pyright" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-    end,
-  },
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				settings = {
+					["lua_ls"] = {},
+				},
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({
+				settings = {
+					["clangd"] = {},
+				},
+				capabilities = capabilities,
+			})
+			lspconfig.jinja_lsp.setup({
+				settings = {
+					["jinja_lsp"] = {},
+				},
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				settings = {
+					["html"] = {},
+				},
+				capabilities = capabilities,
+			})
+			lspconfig.ts_ls.setup({
+				settings = {
+					["ts_ls"] = {},
+				},
+				capabilities = capabilities,
+			})
+			lspconfig.pyright.setup({
+				settings = {
+					["pyright"] = {},
+				},
+				capabilities = capabilities,
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = { "python", "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+      auto_install = true,
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },  
